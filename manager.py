d7403288c0787f02c862a88a1ee20ccd56087630
Jung Chan <jungchan2013@gmail.com>
2020-03-25 11:41:43 +0800

    First version of the code See todos for all the issues that need fixing Doing research on how to make GUI with python backend

--- manager.py
+++ manager.py
@@ -11,6 +11,16 @@ from datetime import date
 # using the scryfall api
 # print out the file to an excel dock
 
+#todo add check for foiling
+#todo add check for
+#todo handle formatting issues when writing to excel
+#todo if completely rewrites the excel file, need to fix that
+#todo convert price text to number
+#todo shorter names will get the wrong similar longer names, e.g. berserk -> vulshok berserker
+#todo weird bug where sol ring and birds of paradise is picking up the foreign black border version
+
+
+
 
 def main():
     today = date.today()
@@ -21,24 +31,56 @@ def main():
     for df in dfs:
         print(dfs[df].head())
 
-    output_dict = []
-    for index, row in dfs["FULL COLLECTION"].iterrows():
-        print("getting... " + row["Name"])
+    # todo this will be an implementation of the specific case of full collection, I will make this generic in future
+
+    input_dict = dfs["FULL COLLECTION"].fillna('').to_dict('records') #this converts it into list of dictionaries
+
+    output_dicts = []
+
+    for dict_entry in input_dict:
+        print("getting... " + dict_entry["Name"])
         start = time.time()
 
         # get list of all editions from scryfall
-        response = requests.get("https://api.scryfall.com/cards/search?q=" + row["Name"].replace(' ', '+') + "&unique=prints").json()
+        response = requests.get("https://api.scryfall.com/cards/search?q=" + dict_entry["Name"].replace(' ', '+') + "&unique=prints").json()
 
-        if len(response["data"]) == 0:
-            print("uh oh, you made a typo for card " + row["Name"] + ", and we can't find it!")
+        if response['object'] == 'error':
+            print("uh oh, you made a typo for card " + dict_entry["Name"] + ", and we can't find it!")
+            output_dicts.append(dict_entry)
             continue
 
-        # we care about name, set, set_name, price, date we accessed.
-        output_dict.append({})
+        response_data = response['data']
 
-        end = time.time()
+        # we care about name, set, set_name, price, date we accessed.
+
+        # this section here helps us to extract the appropriate printing
+        card_data = {} #this stores the card data that matches the version we want
+        if 'Set' in dict_entry and len(dict_entry['Set']) != 0:
+            for dict in response_data:
+                if dict['set'].upper() == dict_entry['Set'].upper().strip():
+                    card_data = dict
+                    break
+        else:
+            min = float('inf')
+            for dict in response_data:
+                if dict['prices']['usd'] is not None:
+                    if float(dict['prices']['usd']) < min:
+                        card_data = dict
+
+        dict_entry['Set'] = card_data['set'].upper()
+        dict_entry['Price'] = card_data['prices']['usd']
+        dict_entry['Name'] = card_data['name']
+        dict_entry['last_accessed'] = fdate
+
+        print(dict_entry)
+
+        output_dicts.append(dict_entry)
+
         print("time taken for this query is (in ms):")
-        print(end - start)
+        print(time.time() - start)
+
+
+    pd.DataFrame(output_dicts).to_excel("Full collection.xlsx", sheet_name="FULL COLLECTION")
 
 
 
